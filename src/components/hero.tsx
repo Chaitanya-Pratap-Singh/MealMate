1134e31acda98320393aca2c1ef08f991011b07f
Chaitanya-Pratap-Singh <chaitanyapratapsingh24@gmail.com>
2025-03-02 21:14:34 +0530

    basic homepage ui setup

---
+++ src/components/hero.tsx
@@ -0,0 +1,37 @@
+/** @format */
+
+"use client";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+export function Hero() {
+	const words = [
+		{
+			text: "Cook",
+		},
+		{
+			text: "awesome",
+		},
+		{
+			text: "food",
+		},
+		{
+			text: "with",
+		},
+		{
+			text: "MealMate.",
+			className: "text-[#F9C2C2] dark:text-[#F9C2C2]",
+		},
+	];
+	return (
+		<div className="flex flex-col items-center justify-center h-[40rem]  ">
+			<p className="text-neutral-600 dark:text-[#FBF8F6] text-xs sm:text-base  ">
+				Your smart companion for delicious meal ideas!
+			</p>
+			<TypewriterEffectSmooth words={words} />
+			<div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+				<button className="w-40 h-10 rounded-xl bg-[#EE5F4C] border dark:border-white border-transparent text-[#FBF8F6] text-sm cursor-pointer">
+					Start Cooking
+				</button>
+			</div>
+		</div>
+	);
+}
